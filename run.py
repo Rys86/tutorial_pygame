201113514e3102b94b4323f2a193493328e93e64
Rys86 <turkowski.mariusz@gmail.com>
2024-11-18 22:29:44 +0100

    Delta Time (clock tick) added

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import pygame, sys
 
+max_tps = 70.0
 pygame.init()
 res = (1280,720)
 screen =  pygame.display.set_mode(res)
 box = pygame.Rect(10,10,50,50)
-x = 10
+# x = 10
+clock = pygame.time.Clock()
+delta = 0.0
 
 while True:
     # Handle Events
@@ -14,18 +17,21 @@ while True:
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             sys.exit(0)
             # box.x += 100
+    # Ticking
+    delta += clock.tick()/1000.0
+    while delta > 1/max_tps:
+        delta -= 1/max_tps
 
-
-    # Checking Input
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
-        box.x += 1
-    if keys[pygame.K_a]:
-        box.x -= 1
-    if keys[pygame.K_w]:
-        box.y -= 1
-    if keys[pygame.K_s]:
-        box.y += 1
+        # Checking Input
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            box.x += 1
+        if keys[pygame.K_a]:
+            box.x -= 1
+        if keys[pygame.K_w]:
+            box.y -= 1
+        if keys[pygame.K_s]:
+            box.y += 1
 
     # Drawing
     screen.fill((0,0,0))
